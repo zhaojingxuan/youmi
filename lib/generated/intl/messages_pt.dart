f26bf60294bec939f48a4754fb896bbe77dfa6b8
zhaojingxuan <zhaojingxuan@pengwin.com>
2025-03-24 13:38:59 +0800

    xx

--- lib/generated/intl/messages_pt.dart
+++ lib/generated/intl/messages_pt.dart
@@ -22,182 +22,180 @@ class MessageLookup extends MessageLookupByLibrary {
 
   final messages = _notInlinedMessages(_notInlinedMessages);
   static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
-        "about": MessageLookupByLibrary.simpleMessage("Sobre"),
-        "account_cancellation": MessageLookupByLibrary.simpleMessage(
-          "Cancelar Conta",
-        ),
-        "ad_not_ready": MessageLookupByLibrary.simpleMessage(
-          "Anúncio não está pronto",
-        ),
-        "affiliation": MessageLookupByLibrary.simpleMessage("Afiliação"),
-        "agreement_desc": MessageLookupByLibrary.simpleMessage(
-          "Ao continuar, você concorda com os seguintes termos",
-        ),
-        "anthology_list": MessageLookupByLibrary.simpleMessage(
-          "Lista de Episódios",
-        ),
-        "auto_renew_cancel_anytime": MessageLookupByLibrary.simpleMessage(
-          "· Renovação automática · Cancele a qualquer momento",
-        ),
-        "automatic_release": MessageLookupByLibrary.simpleMessage(
-          "Desbloqueio Automático",
-        ),
-        "automatically_use_coins_unlock_episodes":
-            MessageLookupByLibrary.simpleMessage(
+    "about": MessageLookupByLibrary.simpleMessage("Sobre"),
+    "account_cancellation": MessageLookupByLibrary.simpleMessage(
+      "Cancelar Conta",
+    ),
+    "ad_not_ready": MessageLookupByLibrary.simpleMessage(
+      "Anúncio não está pronto",
+    ),
+    "affiliation": MessageLookupByLibrary.simpleMessage("Afiliação"),
+    "agreement_desc": MessageLookupByLibrary.simpleMessage(
+      "Ao continuar, você concorda com os seguintes termos",
+    ),
+    "anthology_list": MessageLookupByLibrary.simpleMessage(
+      "Lista de Episódios",
+    ),
+    "auto_renew_cancel_anytime": MessageLookupByLibrary.simpleMessage(
+      "· Renovação automática · Cancele a qualquer momento",
+    ),
+    "automatic_release": MessageLookupByLibrary.simpleMessage(
+      "Desbloqueio Automático",
+    ),
+    "automatically_use_coins_unlock_episodes":
+        MessageLookupByLibrary.simpleMessage(
           "Usar moedas automaticamente para desbloquear episódios",
         ),
-        "bill": MessageLookupByLibrary.simpleMessage("Conta"),
-        "can_unlock_now": MessageLookupByLibrary.simpleMessage(
-          "Você pode desbloquear agora",
-        ),
-        "cancel": MessageLookupByLibrary.simpleMessage("Cancelar"),
-        "check_in": MessageLookupByLibrary.simpleMessage("Check-in"),
-        "check_the_network_and_try_again": MessageLookupByLibrary.simpleMessage(
-          "Verifique sua conexão e tente novamente",
-        ),
-        "click_to_continue": MessageLookupByLibrary.simpleMessage(
-          "Clique para continuar jogando",
-        ),
-        "clip_coins": MessageLookupByLibrary.simpleMessage("Moedas"),
-        "clip_complete": MessageLookupByLibrary.simpleMessage("Concluído"),
-        "clip_detail": MessageLookupByLibrary.simpleMessage("Detalhes"),
-        "clip_drama_hot":
-            MessageLookupByLibrary.simpleMessage("ClipDrama Em Alta"),
-        "clip_ep": MessageLookupByLibrary.simpleMessage("EP."),
-        "clip_get": MessageLookupByLibrary.simpleMessage("Obter"),
-        "clip_less": MessageLookupByLibrary.simpleMessage("Menos"),
-        "clip_more": MessageLookupByLibrary.simpleMessage("Mais"),
-        "clip_popular": MessageLookupByLibrary.simpleMessage("Popular"),
-        "clip_restore": MessageLookupByLibrary.simpleMessage("Restaurar"),
-        "clip_sure": MessageLookupByLibrary.simpleMessage("Certeza"),
-        "clip_vigor": MessageLookupByLibrary.simpleMessage("Energia"),
-        "clip_vip": MessageLookupByLibrary.simpleMessage("VIP"),
-        "collect": MessageLookupByLibrary.simpleMessage("Salvar"),
-        "congratulations": MessageLookupByLibrary.simpleMessage("Parabéns"),
-        "daily_check": MessageLookupByLibrary.simpleMessage("Check-in Diário"),
-        "daily_task": MessageLookupByLibrary.simpleMessage("Tarefas Diárias"),
-        "day": MessageLookupByLibrary.simpleMessage("Dia"),
-        "day_format": MessageLookupByLibrary.simpleMessage("Dia %d"),
-        "delete_account_tips": MessageLookupByLibrary.simpleMessage(
-          "Excluir sua conta removerá permanentemente seu conteúdo e assinaturas. Se deseja excluir sua conta, envie um e-mail de solicitação para nós e responderemos em 24 horas. Obrigado pelo seu apoio.",
-        ),
-        "do_not_miss_out": MessageLookupByLibrary.simpleMessage(
-          "As histórias no meio também são interessantes. Não perca!",
-        ),
-        "double_energy": MessageLookupByLibrary.simpleMessage("Energia Dupla"),
-        "double_energy_desc": MessageLookupByLibrary.simpleMessage(
-          "Energia ganha dentro do tempo efetivo *2",
-        ),
-        "double_exp": MessageLookupByLibrary.simpleMessage("EXP Duplo"),
-        "double_exp_desc": MessageLookupByLibrary.simpleMessage(
-          "Experiência ganha dentro do tempo efetivo *2",
-        ),
-        "earn_more_rewards": MessageLookupByLibrary.simpleMessage(
-          "Ganhe mais recompensas",
-        ),
-        "email_format_error": MessageLookupByLibrary.simpleMessage(
-          "Formato de E-mail Incorreto",
-        ),
-        "enable_auto": MessageLookupByLibrary.simpleMessage("Ativar Auto"),
-        "favorites": MessageLookupByLibrary.simpleMessage("Favoritos"),
-        "go": MessageLookupByLibrary.simpleMessage("Ir"),
-        "guess_you_like":
-            MessageLookupByLibrary.simpleMessage("Você Pode Gostar"),
-        "help_and_feedback": MessageLookupByLibrary.simpleMessage(
-          "Ajuda e Feedback",
-        ),
-        "history": MessageLookupByLibrary.simpleMessage("Histórico"),
-        "language": MessageLookupByLibrary.simpleMessage("Idioma"),
-        "like": MessageLookupByLibrary.simpleMessage("Curtir"),
-        "limited_time": MessageLookupByLibrary.simpleMessage("Tempo Limitado"),
-        "limited_time_offer": MessageLookupByLibrary.simpleMessage(
-          "Oferta por Tempo Limitado",
-        ),
-        "log_out": MessageLookupByLibrary.simpleMessage("Sair"),
-        "login_with_facebook": MessageLookupByLibrary.simpleMessage(
-          "Entrar com Facebook",
-        ),
-        "login_with_google": MessageLookupByLibrary.simpleMessage(
-          "Entrar com Google",
-        ),
-        "main_discover": MessageLookupByLibrary.simpleMessage("Descobrir"),
-        "main_mine": MessageLookupByLibrary.simpleMessage("Meu"),
-        "main_shorts": MessageLookupByLibrary.simpleMessage("Shorts"),
-        "main_store": MessageLookupByLibrary.simpleMessage("Loja"),
-        "member": MessageLookupByLibrary.simpleMessage("Membro"),
-        "network_error": MessageLookupByLibrary.simpleMessage("Erro de Rede"),
-        "no_ads": MessageLookupByLibrary.simpleMessage("· Sem anúncios"),
-        "not_ready": MessageLookupByLibrary.simpleMessage("Não Pronto"),
-        "ok": MessageLookupByLibrary.simpleMessage("OK"),
-        "payment_tips_1": MessageLookupByLibrary.simpleMessage(
-          "1.Pagamento: Após a confirmação da compra e pagamento pelo usuário, o valor será creditado na conta do iTunes.",
-        ),
-        "payment_tips_2": MessageLookupByLibrary.simpleMessage(
-          "2.Cancelar renovação: Este é um serviço de renovação automática. Para cancelar a renovação, o usuário pode desativar manualmente a função de renovação automática nas configurações do iTunes/AppleID a qualquer momento até 24 horas antes do término do período atual de assinatura. Após o cancelamento, não haverá mais cobranças.",
-        ),
-        "please_enter_your_email": MessageLookupByLibrary.simpleMessage(
-          "Por favor, insira seu e-mail",
-        ),
-        "popular_now": MessageLookupByLibrary.simpleMessage("Popular Agora"),
-        "privacy_policy": MessageLookupByLibrary.simpleMessage(
-          "Política de Privacidade",
-        ),
-        "prop_shop": MessageLookupByLibrary.simpleMessage("Loja de Itens"),
-        "s_no_more": MessageLookupByLibrary.simpleMessage("Não há mais dados"),
-        "see_offer": MessageLookupByLibrary.simpleMessage("Ver Oferta"),
-        "settings": MessageLookupByLibrary.simpleMessage("Configurações"),
-        "sign_in": MessageLookupByLibrary.simpleMessage("Entrar"),
-        "subs_management": MessageLookupByLibrary.simpleMessage(
-          "Gerenciamento de Assinatura Google",
-        ),
-        "subs_management_desc": MessageLookupByLibrary.simpleMessage(
-          "Se você deseja gerenciar sua assinatura, por favor acesse",
-        ),
-        "sure_to_log_out": MessageLookupByLibrary.simpleMessage(
-          "Tem certeza que deseja sair?",
-        ),
-        "tips": MessageLookupByLibrary.simpleMessage("Dicas"),
-        "to_do_list": MessageLookupByLibrary.simpleMessage("Lista de Tarefas"),
-        "top_up": MessageLookupByLibrary.simpleMessage("Recarregar"),
-        "try_again": MessageLookupByLibrary.simpleMessage("Tentar Novamente"),
-        "unlimited_access_premium": MessageLookupByLibrary.simpleMessage(
-          "· Acesso ilimitado a todas as séries",
-        ),
-        "unlimited_access_weekly": MessageLookupByLibrary.simpleMessage(
-          "Acesso ilimitado a todas as séries por 1 semana",
-        ),
-        "unlimited_access_year": MessageLookupByLibrary.simpleMessage(
-          "Acesso ilimitado a todas as séries por 1 ano",
-        ),
-        "unlock": MessageLookupByLibrary.simpleMessage("Desbloquear"),
-        "unlock_conditions": MessageLookupByLibrary.simpleMessage(
-          "Condições de desbloqueio:",
-        ),
-        "unlock_now": MessageLookupByLibrary.simpleMessage("Desbloquear Agora"),
-        "upgrade_to_lv_still_needed_xp": MessageLookupByLibrary.simpleMessage(
-          "Precisa de %2\$s XP para subir para o Nível %1\$s",
-        ),
-        "user_agreement": MessageLookupByLibrary.simpleMessage("Termos de Uso"),
-        "vip_subscription_service": MessageLookupByLibrary.simpleMessage(
-          "Serviço de Assinatura VIP",
-        ),
-        "warm_reminder": MessageLookupByLibrary.simpleMessage(
-          "Lembrete Importante",
-        ),
-        "watch_ad_unlock": MessageLookupByLibrary.simpleMessage(
-          "Assista ao anúncio para desbloquear",
-        ),
-        "watch_ads": MessageLookupByLibrary.simpleMessage("Assistir Anúncios"),
-        "watch_ads_to_get_vigor_unlocked_for_free":
-            MessageLookupByLibrary.simpleMessage(
+    "bill": MessageLookupByLibrary.simpleMessage("Conta"),
+    "can_unlock_now": MessageLookupByLibrary.simpleMessage(
+      "Você pode desbloquear agora",
+    ),
+    "cancel": MessageLookupByLibrary.simpleMessage("Cancelar"),
+    "check_in": MessageLookupByLibrary.simpleMessage("Check-in"),
+    "check_the_network_and_try_again": MessageLookupByLibrary.simpleMessage(
+      "Verifique sua conexão e tente novamente",
+    ),
+    "click_to_continue": MessageLookupByLibrary.simpleMessage(
+      "Clique para continuar jogando",
+    ),
+    "clip_coins": MessageLookupByLibrary.simpleMessage("Moedas"),
+    "clip_complete": MessageLookupByLibrary.simpleMessage("Concluído"),
+    "clip_detail": MessageLookupByLibrary.simpleMessage("Detalhes"),
+    "clip_drama_hot": MessageLookupByLibrary.simpleMessage("ClipDrama Em Alta"),
+    "clip_ep": MessageLookupByLibrary.simpleMessage("EP."),
+    "clip_get": MessageLookupByLibrary.simpleMessage("Obter"),
+    "clip_less": MessageLookupByLibrary.simpleMessage("Menos"),
+    "clip_more": MessageLookupByLibrary.simpleMessage("Mais"),
+    "clip_popular": MessageLookupByLibrary.simpleMessage("Popular"),
+    "clip_restore": MessageLookupByLibrary.simpleMessage("Restaurar"),
+    "clip_sure": MessageLookupByLibrary.simpleMessage("Certeza"),
+    "clip_vigor": MessageLookupByLibrary.simpleMessage("Energia"),
+    "clip_vip": MessageLookupByLibrary.simpleMessage("VIP"),
+    "collect": MessageLookupByLibrary.simpleMessage("Salvar"),
+    "congratulations": MessageLookupByLibrary.simpleMessage("Parabéns"),
+    "daily_check": MessageLookupByLibrary.simpleMessage("Check-in Diário"),
+    "daily_task": MessageLookupByLibrary.simpleMessage("Tarefas Diárias"),
+    "day": MessageLookupByLibrary.simpleMessage("Dia"),
+    "day_format": MessageLookupByLibrary.simpleMessage("Dia %d"),
+    "delete_account_tips": MessageLookupByLibrary.simpleMessage(
+      "Excluir sua conta removerá permanentemente seu conteúdo e assinaturas. Se deseja excluir sua conta, envie um e-mail de solicitação para nós e responderemos em 24 horas. Obrigado pelo seu apoio.",
+    ),
+    "do_not_miss_out": MessageLookupByLibrary.simpleMessage(
+      "As histórias no meio também são interessantes. Não perca!",
+    ),
+    "double_energy": MessageLookupByLibrary.simpleMessage("Energia Dupla"),
+    "double_energy_desc": MessageLookupByLibrary.simpleMessage(
+      "Energia ganha dentro do tempo efetivo *2",
+    ),
+    "double_exp": MessageLookupByLibrary.simpleMessage("EXP Duplo"),
+    "double_exp_desc": MessageLookupByLibrary.simpleMessage(
+      "Experiência ganha dentro do tempo efetivo *2",
+    ),
+    "earn_more_rewards": MessageLookupByLibrary.simpleMessage(
+      "Ganhe mais recompensas",
+    ),
+    "email_format_error": MessageLookupByLibrary.simpleMessage(
+      "Formato de E-mail Incorreto",
+    ),
+    "enable_auto": MessageLookupByLibrary.simpleMessage("Ativar Auto"),
+    "favorites": MessageLookupByLibrary.simpleMessage("Favoritos"),
+    "go": MessageLookupByLibrary.simpleMessage("Ir"),
+    "guess_you_like": MessageLookupByLibrary.simpleMessage("Você Pode Gostar"),
+    "help_and_feedback": MessageLookupByLibrary.simpleMessage(
+      "Ajuda e Feedback",
+    ),
+    "history": MessageLookupByLibrary.simpleMessage("Histórico"),
+    "language": MessageLookupByLibrary.simpleMessage("Idioma"),
+    "like": MessageLookupByLibrary.simpleMessage("Curtir"),
+    "limited_time": MessageLookupByLibrary.simpleMessage("Tempo Limitado"),
+    "limited_time_offer": MessageLookupByLibrary.simpleMessage(
+      "Oferta por Tempo Limitado",
+    ),
+    "log_out": MessageLookupByLibrary.simpleMessage("Sair"),
+    "login_with_facebook": MessageLookupByLibrary.simpleMessage(
+      "Entrar com Facebook",
+    ),
+    "login_with_google": MessageLookupByLibrary.simpleMessage(
+      "Entrar com Google",
+    ),
+    "main_discover": MessageLookupByLibrary.simpleMessage("Descobrir"),
+    "main_mine": MessageLookupByLibrary.simpleMessage("Meu"),
+    "main_shorts": MessageLookupByLibrary.simpleMessage("Shorts"),
+    "main_store": MessageLookupByLibrary.simpleMessage("Loja"),
+    "member": MessageLookupByLibrary.simpleMessage("Membro"),
+    "network_error": MessageLookupByLibrary.simpleMessage("Erro de Rede"),
+    "no_ads": MessageLookupByLibrary.simpleMessage("· Sem anúncios"),
+    "not_ready": MessageLookupByLibrary.simpleMessage("Não Pronto"),
+    "ok": MessageLookupByLibrary.simpleMessage("OK"),
+    "payment_tips_1": MessageLookupByLibrary.simpleMessage(
+      "1.Pagamento: Após a confirmação da compra e pagamento pelo usuário, o valor será creditado na conta do iTunes.",
+    ),
+    "payment_tips_2": MessageLookupByLibrary.simpleMessage(
+      "2.Cancelar renovação: Este é um serviço de renovação automática. Para cancelar a renovação, o usuário pode desativar manualmente a função de renovação automática nas configurações do iTunes/AppleID a qualquer momento até 24 horas antes do término do período atual de assinatura. Após o cancelamento, não haverá mais cobranças.",
+    ),
+    "please_enter_your_email": MessageLookupByLibrary.simpleMessage(
+      "Por favor, insira seu e-mail",
+    ),
+    "popular_now": MessageLookupByLibrary.simpleMessage("Popular Agora"),
+    "privacy_policy": MessageLookupByLibrary.simpleMessage(
+      "Política de Privacidade",
+    ),
+    "prop_shop": MessageLookupByLibrary.simpleMessage("Loja de Itens"),
+    "s_no_more": MessageLookupByLibrary.simpleMessage("Não há mais dados"),
+    "see_offer": MessageLookupByLibrary.simpleMessage("Ver Oferta"),
+    "settings": MessageLookupByLibrary.simpleMessage("Configurações"),
+    "sign_in": MessageLookupByLibrary.simpleMessage("Entrar"),
+    "subs_management": MessageLookupByLibrary.simpleMessage(
+      "Gerenciamento de Assinatura Google",
+    ),
+    "subs_management_desc": MessageLookupByLibrary.simpleMessage(
+      "Se você deseja gerenciar sua assinatura, por favor acesse",
+    ),
+    "sure_to_log_out": MessageLookupByLibrary.simpleMessage(
+      "Tem certeza que deseja sair?",
+    ),
+    "tips": MessageLookupByLibrary.simpleMessage("Dicas"),
+    "to_do_list": MessageLookupByLibrary.simpleMessage("Lista de Tarefas"),
+    "top_up": MessageLookupByLibrary.simpleMessage("Recarregar"),
+    "try_again": MessageLookupByLibrary.simpleMessage("Tentar Novamente"),
+    "unlimited_access_premium": MessageLookupByLibrary.simpleMessage(
+      "· Acesso ilimitado a todas as séries",
+    ),
+    "unlimited_access_weekly": MessageLookupByLibrary.simpleMessage(
+      "Acesso ilimitado a todas as séries por 1 semana",
+    ),
+    "unlimited_access_year": MessageLookupByLibrary.simpleMessage(
+      "Acesso ilimitado a todas as séries por 1 ano",
+    ),
+    "unlock": MessageLookupByLibrary.simpleMessage("Desbloquear"),
+    "unlock_conditions": MessageLookupByLibrary.simpleMessage(
+      "Condições de desbloqueio:",
+    ),
+    "unlock_now": MessageLookupByLibrary.simpleMessage("Desbloquear Agora"),
+    "upgrade_to_lv_still_needed_xp": MessageLookupByLibrary.simpleMessage(
+      "Precisa de %2\$s XP para subir para o Nível %1\$s",
+    ),
+    "user_agreement": MessageLookupByLibrary.simpleMessage("Termos de Uso"),
+    "vip_subscription_service": MessageLookupByLibrary.simpleMessage(
+      "Serviço de Assinatura VIP",
+    ),
+    "warm_reminder": MessageLookupByLibrary.simpleMessage(
+      "Lembrete Importante",
+    ),
+    "watch_ad_unlock": MessageLookupByLibrary.simpleMessage(
+      "Assista ao anúncio para desbloquear",
+    ),
+    "watch_ads": MessageLookupByLibrary.simpleMessage("Assistir Anúncios"),
+    "watch_ads_to_get_vigor_unlocked_for_free":
+        MessageLookupByLibrary.simpleMessage(
           "Assista a anúncios para desbloquear gratuitamente",
         ),
-        "watch_full_episodes": MessageLookupByLibrary.simpleMessage(
-          "Assistir Episódios Completos",
-        ),
-        "watch_now": MessageLookupByLibrary.simpleMessage("Assistir Agora"),
-        "weekly_vip_title": MessageLookupByLibrary.simpleMessage(
-          "Desconto na Primeira Semana do Membro Semanal",
-        ),
-      };
+    "watch_full_episodes": MessageLookupByLibrary.simpleMessage(
+      "Assistir Episódios Completos",
+    ),
+    "watch_now": MessageLookupByLibrary.simpleMessage("Assistir Agora"),
+    "weekly_vip_title": MessageLookupByLibrary.simpleMessage(
+      "Desconto na Primeira Semana do Membro Semanal",
+    ),
+  };
 }
